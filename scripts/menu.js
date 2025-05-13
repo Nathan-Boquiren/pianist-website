0f566053812fed42e8ed2a4198f0d503ee62edd3
Nathan Boquiren <nathanjboquiren@gmail.com>
2025-05-12 20:55:18 -0400

    implement responsive hamburger menu and update styles

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,1 +1,18 @@
-let cl = console.log;
+// Menu DOM Elements
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("nav-menu");
+
+let menuOpen = false;
+
+hamburger.addEventListener("click", (e) => {
+  e.stopPropagation();
+  hamburger.classList.toggle("open");
+  menuOpen = true;
+});
+
+document.body.addEventListener("click", (e) => {
+  if (menuOpen && !menu.contains(e.target) && e.target !== hamburger) {
+    hamburger.classList.remove("open");
+    menuOpen = false;
+  }
+});
